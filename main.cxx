baa936c981a62bfbd5274b35df63ec77c7778f94
“shilparani <shilpa_rani.sahu@sljus.lu.se>
2023-10-04 16:51:59 +0200

    changed the variable name var0 and var1 to argc and argv

--- main.cxx
+++ main.cxx
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <string>
-int main(int var0, char *var1[]) {
-  if (var0 == 3) {
-    std::string var2{var1[0]};
-    auto var3{*(var1[1])};
+int main(int argc, char *argv[]) {
+  if (argc == 3) {
+    std::string var2{argv[0]};
+    auto var3{*(argv[1])};
     auto var4{var2.size()};
-    auto var5{std::atoi(var1[2])};
+    auto var5{std::atoi(argv[2])};
     auto var6{0};
     auto var7{0};
-    std::string var8{var1[1]};
+    std::string var8{argv[1]};
     while (true) {
       var6 += var8[var7++];
       if (var7 >= static_cast<int>(var8.size())) {
